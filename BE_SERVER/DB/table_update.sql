9cc588884092bf6864dd0bd767ed2a58c101637d
Youngjin-Son <yjyjson12@gmail.com>
2021-08-13 18:09:25 +0900

    책장 스캐닝 후 테이블 업데이트

--- BE_SERVER/DB/table_update.sql
+++ BE_SERVER/DB/table_update.sql
@@ -1,3 +1,6 @@
-Create table book_tmp ( select * from book ORDER BY FIELD(Book_ID, 295,121,175,289,201,169,144,272,199,258));
+Create table book_tmp ( select * from book ORDER BY FIELD(Book_ID, 121,295,175,289,201,169,144,272,199,258));
 DROP table book;
 Create table book (select * from book_tmp);
+DROP table book_tmp;
+
+
